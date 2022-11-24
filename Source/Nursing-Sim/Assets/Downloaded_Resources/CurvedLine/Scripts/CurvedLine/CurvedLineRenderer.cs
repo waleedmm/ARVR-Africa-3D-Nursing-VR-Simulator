eb8b058263fc8daf9a64c00d4c4419c353685a56
waleed8 <waleed.m.m@gmail.com>
2022-11-24 15:12:32 +0200

    Contest-0 (base files)

---
+++ Source/Nursing-Sim/Assets/Downloaded_Resources/CurvedLine/Scripts/CurvedLine/CurvedLineRenderer.cs
@@ -0,0 +1,98 @@
+﻿// Code from https://forum.unity.com/threads/easy-curved-line-renderer-free-utility.391219/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent( typeof(LineRenderer) )]
+public class CurvedLineRenderer : MonoBehaviour 
+{
+	//PUBLIC
+	public float lineSegmentSize = 0.15f;
+	[Header("Gizmos")]
+	public bool showGizmos = true;    
+    public bool isNeedToSmooth = true;
+	public float gizmoSize = 0.1f;
+	public Color gizmoColor = new Color(1,0,0,0.5f);
+	//PRIVATE
+	private CurvedLinePoint[] linePoints = new CurvedLinePoint[0];
+	private Vector3[] linePositions = new Vector3[0];
+	private Vector3[] linePositionsOld = new Vector3[0];
+
+     // Update is called once per frame
+    public void LateUpdate () 
+	{
+		GetPoints();
+		SetPointsToLine();
+	}
+
+	void GetPoints()
+	{        
+		//find curved points in children
+		linePoints = this.GetComponentsInChildren<CurvedLinePoint>();
+
+		//add positions
+		linePositions = new Vector3[linePoints.Length];
+		for( int i = 0; i < linePoints.Length; i++ )
+		{
+			linePositions[i] = linePoints[i].transform.position;
+		}
+	}
+
+	void SetPointsToLine()
+	{
+		//create old positions if they dont match
+		if( linePositionsOld.Length != linePositions.Length )
+		{
+			linePositionsOld = new Vector3[linePositions.Length];
+		}
+
+		//check if line points have moved
+		bool moved = false;
+		for( int i = 0; i < linePositions.Length; i++ )
+		{
+			//compare
+			if( linePositions[i] != linePositionsOld[i] )
+			{
+				moved = true; break;
+			}
+		}
+		//update if moved
+		if( moved == true )
+		{            
+			LineRenderer line = this.GetComponent<LineRenderer>();
+            //line.useWorldSpace = false;
+            //get smoothed values
+            Vector3[] smoothedPoints = linePositions;
+
+            if (isNeedToSmooth)
+                smoothedPoints = LineSmoother.SmoothLine( linePositions, lineSegmentSize );
+
+			//set line settings
+			line.positionCount = smoothedPoints.Length;
+			line.SetPositions( smoothedPoints );
+
+        }
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		LateUpdate();
+	}
+
+	void OnDrawGizmos()
+	{
+		if( linePoints.Length == 0 )
+		{
+			GetPoints();
+		}
+
+		//settings for gizmos
+		foreach( CurvedLinePoint linePoint in linePoints )
+		{
+			linePoint.showGizmo = showGizmos;
+			linePoint.gizmoSize = gizmoSize;
+			linePoint.gizmoColor = gizmoColor;
+		}
+	}
+}
